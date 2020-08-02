f6018dfb369b4ac5a79b06fcff8df699487a3c8c
Joseph Asante <freecoding1@gmail.com>
2020-08-02 23:15:11 +0000

    App.js and Index.css committed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="grid-container">
       <header> <a href="/">React Shopping Cart</a> </header>
         <main> Product List </main>
-        <footer> All right is reserved..</footer>
+        <footer> All right is reserved.</footer>
      
 
     </div>
